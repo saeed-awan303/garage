ffd8280cdec3bc1df97975f8897614b0d86ab58c
umair982 <umair.developer982@gmail.com>
2023-06-17 19:31:12 +0500

    dashboard graph related changes

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -146,28 +146,12 @@
 					<div class="card card-custom card-stretch card-stretch-half gutter-b">
 						<!--begin::Body-->
 						<div class="card-body p-0">
-							<div class="d-flex align-items-center justify-content-between card-spacer flex-grow-1">
-													<span class="symbol symbol-50 symbol-light-success mr-2">
-														<span class="symbol-label">
-															<span class="svg-icon svg-icon-xl svg-icon-success">
-																<!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
-																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																		<rect x="0" y="0" width="24" height="24" />
-																		<rect fill="#000000" x="4" y="4" width="7" height="7" rx="1.5" />
-																		<path d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z" fill="#000000" opacity="0.3" />
-																	</g>
-																</svg>
-																<!--end::Svg Icon-->
-															</span>
-														</span>
-													</span>
-								{{-- <div class="d-flex flex-column text-right">
-									<span class="text-dark-75 font-weight-bolder font-size-h3">{{ $orderCount }}</span>
-									<span class="text-muted font-weight-bold mt-2">Orders</span>
-								</div> --}}
-                                <canvas id="orderChart"></canvas>
+							<div class="row">
+								<div class="col-12">
+									<canvas id="orderChart"></canvas>
+								</div>
 							</div>
+
 							{{-- <div id="kt_stats_widget_11_chart" class="card-rounded-bottom" data-color="success" style="height: 150px"></div> --}}
 						</div>
 						<!--end::Body-->
